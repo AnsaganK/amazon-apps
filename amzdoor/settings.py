abc2bd31719e493571cb405c1ffe0899e5b12842
Ansagan-Kabdolla <kabdolla00.00@mail.ru>
2021-09-21 19:20:00 +0600

    add staticfiles_storage in settings

--- amzdoor/settings.py
+++ amzdoor/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 
