565ae7594834bdd237d037715adf69a19026b80b
Ansagan-Kabdolla <kabdolla00.00@mail.ru>
2021-09-21 19:29:06 +0600

    add remove in settings

--- amzdoor/settings.py
+++ amzdoor/settings.py
@@ -136,7 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
